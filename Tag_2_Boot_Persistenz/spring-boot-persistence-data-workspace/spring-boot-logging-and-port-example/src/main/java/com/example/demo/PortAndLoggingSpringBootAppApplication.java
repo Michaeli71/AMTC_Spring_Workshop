f3d2ab21178ae0abc318ce4e51b3b6653b617893
Michael Inden <michael_inden@hotmail.com>
2021-10-06 17:19:49 +0200

    Demo for PORT and Logging

---
+++ Tag_2_Boot_Persistenz/spring-boot-persistence-data-workspace/spring-boot-logging-and-port-example/src/main/java/com/example/demo/PortAndLoggingSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortAndLoggingSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortAndLoggingSpringBootAppApplication.class, args);
+	}
+
+}
