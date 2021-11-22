2d0184590143d769d3de88b96f4e795992ae0b6e
Michael Inden <michael_inden@hotmail.com>
2021-11-22 22:49:53 +0100

    Delete Tag_2_Boot_Persistenz/spring-boot-persistence-data-workspace directory

--- Tag_2_Boot_Persistenz/spring-boot-persistence-data-workspace/ex17-asmiq-academy-boot-app-template/src/main/java/ch/asmiq/interfaces/CertificationService.java
+++
@@ -1,8 +0,0 @@
-package ch.asmiq.interfaces;
-
-import ch.asmiq.model.Order;
-
-public interface CertificationService {
-
-	void downloadCertificate(Order order);
-}
--- Tag_2_Boot_Persistenz/spring-boot-persistence-data-workspace/ex17-asmiq-academy-boot-app-template/src/main/java/ch/asmiq/interfaces/FeedbackService.java
+++
@@ -1,8 +0,0 @@
-package ch.asmiq.interfaces;
-
-import ch.asmiq.model.Order;
-
-public interface FeedbackService {
-
-	void conductSurvey(Order order);
-}
